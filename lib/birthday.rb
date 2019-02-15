b2ac38891d0f6fb09aabb18f8923285d20ad30d5
Lu Daley <daleyluis@gmail.com>
2019-02-15 23:23:08 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,12 @@
+require 'pry'
 # birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
+#   "Timmy" => 9,
+#   "Sarah" => 6,
 #   "Amanda" => 27
 # }
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+#prints out a birthday message to each kid in the birthday_kids
+# add your code snippet here!
+binding.pry
 end
-
-
